d747a3e83513bde8c8fcdf1d5f603843506ca248
naffiziqbal <f.nafiziqbal@gmail.com>
2022-11-30 21:53:38 +0600

    Blog

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 const express = require('express'); //import Express 
 const cors = require('cors')  // import cors 
 const courses = require('./courses.json') // import Course Data
-
+const topCourses = require('./topCourses.json');
+const blog = require('./blog.json');
 
 const app = express();
 app.use(cors());
 
 const port = process.env.PORT || 5000;
 
+
 app.get('/', (req, res) => {
     res.send(" Server Running listen on /courses")
 })
-app.get('/courses', (req, res) => {
 
+
+app.get('/courses', (req, res) => {
     res.send(courses)
 })
+
+
+app.get('/limitCourses', (req, res) => {
+    res.send(topCourses)
+})
+
+
+app.get('/blog', (req, res) => {
+    res.send(blog)
+})
+
+
 app.get('/courses/:id', (req, res) => {
     const iid = req.params.id;
-    // console.log(query);
     const result = courses.find(id => id.id == iid)
     res.send(result)
 })
+
+
 app.get('/checkout/:id', (req, res) => {
     const iid = req.params.id;
     // console.log(query);
